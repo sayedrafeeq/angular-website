a848a42cca04a3494cea38607dbc92c0ddf75598
Rafeeq <sayedrafeeq@gmail.com>
2026-03-16 18:19:54 +0530

    Fix nested router outlet issue - use ng-content in layout component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
